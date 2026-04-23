89ca1aa02b1e57341139c90ff23c37d570eb8f34
Aaron Goldberg <aaron.guerra137@gmail.com>
2026-04-23 20:58:40 +0000

    feat: Adição de novas funcionalidades com Java para executar o Projeto.

--- biblioteca_java/src/test/java/com/example/biblioteca/BibliotecaApplicationTests.java
+++ biblioteca_java/src/test/java/com/example/biblioteca/BibliotecaApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.biblioteca;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.exemplo.biblioteca.BibliotecaApplication;
+
+@SpringBootTest(classes = BibliotecaApplication.class)
 class BibliotecaApplicationTests {
 
 	@Test
